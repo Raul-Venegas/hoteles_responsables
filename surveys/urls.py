c091ad991226d01f47cc1b845319df31f2e8dd7d
RaulMV <raul.martinezv@elektra.com.mx>
2024-11-24 22:21:42 -0600

    Vistas para ir a cuestionarios listas

--- surveys/urls.py
+++ surveys/urls.py
@@ -5,4 +5,10 @@ app_name = 'surveys'
 
 urlpatterns = [
     path('', views.home_surveys, name="home_surveys"),
+    path('natural/', views.natural, name="natural"),
+    path('natural_encuesta/', views.natural_survey, name="natural_survey"),
+    path('socio-cultural/', views.socio_cultural, name="socio-cultural"),
+    path('socio-cultural_encuesta/', views.socio_cultural_survey, name="socio-cultural_survey"),
+    path('economico/', views.economico, name="economico"),
+    path('economico_encuesta/', views.economico_survey, name="economico_survey"),
 ]
--- surveys/views.py
+++ surveys/views.py
@@ -5,3 +5,24 @@ from django.views import View
 
 def home_surveys(request):
     return render(request,'surveys/home_surveys.html')
+
+def natural(request):
+    return render(request,'surveys/natural.html')
+
+@login_required
+def natural_survey(request):
+    return render(request,'surveys/natural_survey.html')
+
+def socio_cultural(request):
+    return render(request,'surveys/socio-cultural.html')
+
+@login_required
+def socio_cultural_survey(request):
+    return render(request,'surveys/socio_cultural_survey.html')
+
+def economico(request):
+    return render(request,'surveys/economico.html')
+
+@login_required
+def economico_survey(request):
+    return render(request,'surveys/economico_survey.html')
